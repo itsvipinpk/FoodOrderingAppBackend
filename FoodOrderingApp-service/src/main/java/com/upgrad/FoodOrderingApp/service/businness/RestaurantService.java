5b29d3f1afd9a2e45953ab3678bf055b39728831
Vipin P K <51770037+itsvipinpk@users.noreply.github.com>
2021-02-02 23:41:52 +0530

    Refactoring of item controller related components

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/RestaurantService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/RestaurantService.java
@@ -2,6 +2,7 @@ package com.upgrad.FoodOrderingApp.service.businness;
 
 import com.upgrad.FoodOrderingApp.service.dao.RestaurantDao;
 import com.upgrad.FoodOrderingApp.service.entity.RestaurantEntity;
+import com.upgrad.FoodOrderingApp.service.exception.RestaurantNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -17,5 +18,20 @@ public class RestaurantService {
 
     public List<RestaurantEntity> getAllRestaurant(){
         return restaurantDao.getAllRestaurant();
+    }
+
+    /**
+     * This method gets the restaurant details.
+     *
+     * @param uuid UUID of the restaurant.
+     * @return
+     * @throws RestaurantNotFoundException if restaurant with UUID doesn't exist in the database.
+     */
+    public RestaurantEntity restaurantByUUID(String uuid) throws RestaurantNotFoundException {
+        RestaurantEntity restaurant = restaurantDao.restaurantByUUID(uuid);
+        if (restaurant == null) {
+            throw new RestaurantNotFoundException("RNF-001", "No restaurant by this id");
+        }
+        return restaurant;
     }
 }
