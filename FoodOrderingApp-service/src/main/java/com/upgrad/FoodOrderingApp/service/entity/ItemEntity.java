8e6966b9142bf78ee8efd80e584ae126cc5600ab
Vipin P K <51770037+itsvipinpk@users.noreply.github.com>
2021-02-05 01:31:03 +0530

    Implemented item controller

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/ItemEntity.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/ItemEntity.java
@@ -1,5 +1,7 @@
 package com.upgrad.FoodOrderingApp.service.entity;
 
+import com.upgrad.FoodOrderingApp.service.common.ItemType;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -11,9 +13,23 @@ import java.util.List;
 @Table(name = "item")
 @NamedQueries(
         {
-                @NamedQuery(name = "getAllItems", query = "select i from ItemEntity i "),
+                @NamedQuery(name = "getAllItems", query = "select i from ItemEntity i ")
         }
 )
+@NamedNativeQueries({
+        // Using native query as named queries do not support LIMIT in nested statements.
+        @NamedNativeQuery(
+                name = "topFivePopularItemsByRestaurant",
+                query =
+                        "select * from item where id in "
+                                + "(select item_id from order_item where order_id in "
+                                + "(select id from orders where restaurant_id = ? ) "
+                                + "group by order_item.item_id "
+                                + "order by (count(order_item.order_id)) "
+                                + "desc LIMIT 5)",
+                resultClass = ItemEntity.class)
+})
+
 public class ItemEntity {
 
     @Id
@@ -88,8 +104,8 @@ public class ItemEntity {
         return type;
     }
 
-    public void setType(String type) {
-        this.type = type;
+    public void setType(ItemType type) {
+        this.type = type.toString();
     }
 
 
